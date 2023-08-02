52329de2d2c13def87f1810f852cd351f68e74ee
Kofa-Yoh <m.v.kotkina@gmail.com>
2023-08-03 00:47:57 +0300

    Delete superfluous

--- src/main/java/com/example/MyBookShopApp/data/BooksRatingAndPopularityRepository.java
+++ src/main/java/com/example/MyBookShopApp/data/BooksRatingAndPopularityRepository.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 public interface BooksRatingAndPopularityRepository extends JpaRepository<Book, Integer> {
 
-    List<Book> findBooksByStatisticNotNullOrderByStatistic_BuyersCount();
-
     @Query("from Book b join BooksStatistic s on b.id = s.book.id where s.buyersCount + s.inCartCount + s.postponedCount > 0 order by s.buyersCount + 0.7 * s.inCartCount + 0.4 * s.postponedCount DESC")
     Page<Book> getBooksOrderedByPopularity(Pageable nextPage);
 }
--- src/main/java/com/example/MyBookShopApp/data/BooksRatingAndPopularityService.java
+++ src/main/java/com/example/MyBookShopApp/data/BooksRatingAndPopularityService.java
@@ -11,20 +11,9 @@ import java.util.List;
 public class BooksRatingAndPopularityService {
 
     private final BooksRatingAndPopularityRepository booksRatingAndPopularityRepository;
-    private final Double IN_CART_RATE = 0.7;
-    private final Double POSTPONED_RATE = 0.4;
 
     public BooksRatingAndPopularityService(BooksRatingAndPopularityRepository booksRatingAndPopularityRepository) {
         this.booksRatingAndPopularityRepository = booksRatingAndPopularityRepository;
-    }
-
-    public Double getBookPopularity(Integer book_id){
-//        BooksStatistic booksStatistic = booksRatingAndPopularityRepository.findBooksStatisticByBook_Id(book_id);
-//        if(booksStatistic == null) return 0.0;
-//        return booksStatistic.getBuyersCount() +
-//                IN_CART_RATE * booksStatistic.getInCartCount() +
-//                POSTPONED_RATE * booksStatistic.getPostponedCount();
-        return 0.0;
     }
 
     public Page<Book> getPopularBooks(Integer offset, Integer limit){
