01e3e81be200cda12825f2a90eb06ab3986dc4ce
Kofa-Yoh <m.v.kotkina@gmail.com>
2023-09-02 08:30:11 +0700

    Task 8_3 Test spring security

---
+++ src/main/java/com/example/MyBookShopApp/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.MyBookShopApp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    private final BookStoreUserDetailService bookStoreUserDetailService;
+
+    @Autowired
+    public SecurityConfig(BookStoreUserDetailService bookStoreUserDetailService) {
+        this.bookStoreUserDetailService = bookStoreUserDetailService;
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    /*@Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(bookStoreUserDetailService);
+        authProvider.setPasswordEncoder(getPasswordEncoder());
+        return authProvider;
+    }*/
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+//                        .requestMatchers("/my", "/profile").hasRole("USER")
+//                        .requestMatchers("/**").permitAll()
+                                .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/signin")
+                        .defaultSuccessUrl("/my", true)
+                        .failureUrl("/signin")
+                        .permitAll()
+                );
+        return http.build();
+    }
+}
