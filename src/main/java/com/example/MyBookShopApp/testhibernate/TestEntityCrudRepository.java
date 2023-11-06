06e3938ddc707d0d6e9b5fb014623ba88b62b18b
Kofa-Yoh <m.v.kotkina@gmail.com>
2023-11-07 03:36:04 +0700

    Delete hibernate test classes

--- src/main/java/com/example/MyBookShopApp/testhibernate/TestEntityCrudRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.MyBookShopApp.testhibernate;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TestEntityCrudRepository extends CrudRepository<TestEntity, Long> {
-}
